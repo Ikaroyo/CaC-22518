42404182ef0dc1e60327dab65263de3d008d0d13
Ikaroyo <emmanuel1631@gmail.com>
2022-09-28 23:50:54 -0300

    Carga 28/09/2022

--- js/script_galeria.js
+++ js/script_galeria.js
@@ -1,43 +1,98 @@
 // Funciones que se ejecutan en carga de la pagina
 window.onload = loadImages;
-window.setInterval(modificarBackground, 1);
+const cantidad = 14;
 
-// Inicializacion de variables
-var grados = 90.0;
-var per1 = 0.0;
-var flagPer1 = 1;
-
-// Funcion de carga de imagenes dentro del div galeria
+// Funcion de carga de imagenes dentro del div carousel-container
 function loadImages() {
     // Ruta de la carpeta de imagenes
     const path = "img/Vehiculos/galeria/";
 
     // Cantidad de imagenes a cargar
-    const cantidad = 43;
+
 
     // Bucle de creacion de elementos dentro del div galeria
     for (var i = 1; i <= cantidad; i++) {
         var img = document.createElement("img");
         img.src = path + i + ".jpg";
         img.className = "imagenes";
+        // add onclick call to bigImage function
+        img.onclick = function () {
+            popupImage(this);
+        }
+
         img.alt = "Imagen " + i;
-        document.getElementById("galeria").appendChild(img);
+        document.getElementById("gallery-container").appendChild(img);
     }
 }
 
-// Funcion de background cambiante
-function modificarBackground() {
-    // actualizacion del estilo del background
-    document.body.style.backgroundImage = 'linear-gradient(' + grados + 'deg, rgb(26, 26, 29) ' + per1 + '%, rgb(111, 34, 50) '+(200-per1)+'%)';
+// Funcion de carga de imagen en el div popup
+function popupImage(img) {
+    var popup = document.getElementById("popup");
+    popup.classList.add("active");
+    popup.style.display = "block";
 
-    // Incremento de grados
-    grados += 0.2;
-    // Cambio de sentido de crecimiento de per1
-    if (per1 >= 50 && flagPer1 === 1) {
-        flagPer1 = -1;
-    }else if(per1 <= -100 && flagPer1 === -1){
-        flagPer1 = 1;
-    }
-    // Incremento de per1
-    per1+=flagPer1;
+    popup.innerHTML = "<img src='" + img.src + "' alt='" + img.alt + "' class='popup-img' onclick='inactivePopup()' />";
+
+    var buttonCarrousel = document.getElementById("button-carrousel-l");
+    buttonCarrousel.classList.add("activeb");
+
+    var buttonCarrousel = document.getElementById("button-carrousel-r");
+    buttonCarrousel.classList.add("activeb");
+
+
 }
+
+function inactivePopup() {
+    var popup = document.getElementById("popup");
+    popup.classList.remove("active");
+    popup.style.display = "none";
+    var buttonCarrousel = document.getElementById("button-carrousel-l");
+    buttonCarrousel.classList.remove("activeb");
+
+    var buttonCarrousel = document.getElementById("button-carrousel-r");
+    buttonCarrousel.classList.remove("activeb");
+}
+
+function carrouselMove(i) {
+    // Obtener la imagen actual
+    var img = document.getElementsByClassName("popup-img")[0];
+    // Obtener el numero de la imagen actual
+    var num = parseInt(img.src.split("/").pop().split(".")[0]);
+    console.log(num);
+
+    // Si el parametro es 1
+
+    if (i == 1) {
+        // Si el numero es menor a la cantidad de imagenes
+        if (num < cantidad) {
+            // Sumar 1 al numero
+            num++;
+        } else {
+            // Si no, asignar 1
+            num = 1;
+        }
+    }
+    // Si el parametro es -1
+    else if (i == -1) {
+        // Si el numero es mayor a 1
+        if (num > 1) {
+            // Restar 1 al numero
+            num--;
+        } else {
+            // Si no, asignar la cantidad de imagenes
+            num = cantidad;
+        }
+    }
+
+    // Obtener la ruta de la imagen
+    var path = img.src.split("/");
+    path.pop();
+    path = path.join("/") + "/";
+    // Cambiar la imagen
+    img.src = path + num + ".jpg";
+    img.alt = "Imagen " + num;
+
+
+
+}
+
