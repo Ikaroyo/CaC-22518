92a031746cf92528fc7e38337adf6e8453d54014
Ikaroyo <emmanuel1631@gmail.com>
2022-10-23 01:21:19 -0300

    Carga 23/10/2022: Overflow-x: hidden, agregado para eliminar un error y otros fixes menores

--- js/script_catalogo.js
+++ js/script_catalogo.js
@@ -1,70 +1,62 @@
 window.onload = function () {
+    // pedido al servidor de los vehiculos
     getVehicles();
-
 };
 
-
+// inicializacion de variables
 let dataFull;
 let dataNew;
 
-// get the data from the server
-function getVehicles() {
 
-    // create the request
+// obtener los vehiculos del servidor
+function getVehicles() {
+    // crear el request
     var request = new XMLHttpRequest();
-    // open the request
+    // abrir el request
     request.open("GET", "https://vehiculos-2b1d3-default-rtdb.firebaseio.com/vehiculos.json", true);
     document.getElementById("loading").classList.add("active");
-    // send the request
+    // enviar el request
     request.send();
-    // when the request is ready
-  request.onreadystatechange = function () {
+    // cuando el request este listo
+    request.onreadystatechange = function () {
         if (request.readyState == 4 && request.status == 200) {
             // get the data
             dataFull = JSON.parse(request.responseText);
             // show the data
             showVehicles(dataFull);
             document.getElementById("loading").classList.remove("active");
-
         }
-
     }
-
-//    showVehicles(dataFull);
-
 }
 
-// show the vehicles
 
+// mostrar los vehiculos
 function showVehicles(data) {
     // clear the table
     clearTable();
-
     // get the table
     var table = document.getElementById("table");
     // get the template
     var template = document.getElementById("template");
-    // for each vehicle
+    // for each vehiculo
     for (var i in data) {
-        // get the vehicle
+        // obtener el vehiculo
         var vehicle = data[i];
-        // clone the template
+        // clonar el template
         var clone = template.cloneNode(true);
-        // show the vehicle
+        // mostrar los vehiculos
         clone.querySelector(".number").textContent = i;
         clone.querySelector(".make").innerHTML = vehicle.make;
         clone.querySelector(".model").innerHTML = vehicle.model;
         clone.querySelector(".year").innerHTML = vehicle.year;
         clone.querySelector(".valor").innerHTML = vehicle.price;
-        // show the clone
+        // mostrar el clon
         table.appendChild(clone);
     }
-
 }
 
-
+// limpiar la tabla
 function clearTable() {
-
     var table = document.getElementById("table");
     var template = document.getElementById("template");
     var rows = table.getElementsByTagName("tr");
@@ -73,24 +65,26 @@ function clearTable() {
         rows[i].parentNode.removeChild(rows[i]);
     }
     table.appendChild(template);
-
 }
 
 
+// Esperar mientras se escribe
+function preChargeAgain() {
+    setTimeout(chargeAgain, 1000);
+}
+
+// funcion de muestra de vehiculos
 function chargeAgain() {
-
-
     dataNew = dataFull.filter(function (vehicle) {
-        // check in lowercase, by make, model year
+        // check en minuscula, make, model y year
         return vehicle.make.toLowerCase().includes(document.getElementById("make").value.toLowerCase()) &&
             vehicle.model.toLowerCase().includes(document.getElementById("model").value.toLowerCase()) && vehicle.year.includes(document.getElementById("year").value);
     });
-
-
     showVehicles(dataNew);
-
 }
 
+
+// limpiar el form
 
 function limpiar() {
     document.getElementById("make").value = "";
